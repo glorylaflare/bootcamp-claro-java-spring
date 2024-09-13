ae637c3a4c27e8f20b5b3666be802dccb42f7aff
Marcelo Gonçalves <juninho_toh@hotmail.com>
2024-09-13 15:32:04 -0300

    feat: adicionando arquivos finais dos desafio de código

---
+++ desafios-de-codigo/src/main/java/org/example/VerificaQuedaDeConexao.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class VerificaQuedaDeConexao {
+    // TODO: Preencha a função para verificar queda de conexão
+    public static String verificarQuedaConexao(String[] velocidades) {
+        for (String velocidade : velocidades) {
+            if ("0".equals(velocidade)) {
+                return "Queda de Conexao";
+            }
+        }
+        return "Sem Quedas";
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitando ao usuário a lista de velocidades de conexão registradas a cada hora
+        String input = scanner.nextLine();
+
+        // Convertendo a entrada em uma lista de strings
+        String[] velocidades = input.split(",");
+
+        // Verificando se houve queda de conexão
+        String resultado = verificarQuedaConexao(velocidades);
+
+        // Exibindo o resultado da verificação
+        System.out.println(resultado);
+
+        scanner.close();
+    }
+}
